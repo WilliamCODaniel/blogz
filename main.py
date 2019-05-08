e8c9f863b74adc308ae6e52d076aab42b943b3c7
William O'Daniel <william.odaniel32@gmail.com>
2019-05-07 20:09:31 -0500

    finished first part of project

--- main.py
+++ main.py
@@ -17,20 +17,43 @@ class Blog(db.Model):
         self.name = name
         self.post = content
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+#@app.route('/')
+#def index():
+    
 
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+    title_error = ''
+    content_error = ''
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = Blog(post_title, post_content)
-        db.session.add(new_post)
-        db.session.commit()
-    
+        if not post_title:
+            title_error = 'No Title entered'
+            post_title = ''      
+        if not post_content:
+            content_error = 'No post content'
+            post_content = ''
+        else:
+            new_post = Blog(post_title, post_content)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/blog')
+
     posts = Blog.query.all()
 
-    return render_template('blog_post.html', title="New Blog Post",
-        posts=posts)
+    if title_error or content_error:
+        return render_template('new_post.html', title="New Blog Post",
+            posts=posts, title_error = title_error, content_error = content_error)
+
+    return render_template('new_post.html', title="New Blog Post")
+
+@app.route('/blog')
+def blog():
+
+    posts = Blog.query.all()
+    return render_template('blog.html', title="Blog Posts", posts=posts)
 
 
 if __name__ =='__main__':
